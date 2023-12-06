ebf9f0eff3ad1e7b5aac84bcb3481c4299ecd0bd
Gulay Selvi <gulayselvi3@gmail.com>
2023-12-06 21:15:26 +0300

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/data/datas.dart';
+import 'package:mealsapp/models/category.dart';
+import 'package:mealsapp/screens/meal_list.dart';
+import 'package:mealsapp/widgets/category_card.dart';
+import 'package:mealsapp/widgets/drawer.dart';
+
+
+class Categories extends StatelessWidget {
+  const Categories({Key? key}) : super(key: key);
+
+//Stful widgetlarda => context globaldir
+//Stless widgetlarda => sadece build fonksiyonunda context'e erişilebilir
+  void _selectCategory(BuildContext context, Category category) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealList(
+            meals: meals
+                .where((element) => element.categoryId == category.id)
+                .toList())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MyDrawer(child: ListView()),
+      appBar: AppBar(
+        title: const Text("Bir kategori seçin"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in categories)
+            CategoryCard(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+
+
+class MyDrawer extends StatefulWidget {
+  const MyDrawer({ Key? key, required ListView child }) : super(key: key);
+
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor:const Color.fromARGB(255, 31, 30, 29),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Color.fromARGB(255, 200, 164, 72), Color.fromARGB(255, 240, 145, 51)]),
+              ),
+            child: Text("Kategoriler"),
+            ),
+            ListTile(
+              title: const Text("Başlangıçlar"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Ara Sıcaklar"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Ana Yemekler"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Tatlılar"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ],)
+    );
+  }
+}
